40a7844fbb80a095e8a7ccf0fbd8ddf37c61ca86
BurnFirework <99072007+BurnFirework@users.noreply.github.com>
2023-04-12 23:41:29 -0700

    improved solution

--- solution.py
+++ solution.py
@@ -9,12 +9,12 @@ def solution(x_success: int,
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    imp_crit = 0.09
+    # imp_crit = 0.09
+    z_critical = 1.340755
     p_control = x_success / x_cnt
     p_test = y_success / y_cnt
     p_combined = (x_success + y_success) / (x_cnt + y_cnt)
 
     se = np.sqrt(p_combined * (1 - p_combined) * (1 / x_cnt + 1 / y_cnt))
     z_score = (p_test - p_control) / se
-    z_critical = abs(norm.ppf(imp_crit / 2))
     return (abs(z_score) > z_critical)
