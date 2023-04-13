3f9aa508711617c4dba4e04fbda5e54fe9bd1ab2
BurnFirework <99072007+BurnFirework@users.noreply.github.com>
2023-04-12 20:54:12 -0700

    updated solution

--- solution.py
+++ solution.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 
+from scipy.stats import norm
 
 chat_id = 860138765
 
@@ -8,7 +9,12 @@ def solution(x_success: int,
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    imp_crit = 0.09
+    p_control = x_success / x_cnt
+    p_test = y_success / y_cnt
+    p_combined = (x_success + y_success) / (x_cnt + y_cnt)
+
+    se = np.sqrt(p_combined * (1 - p_combined) * (1 / x_cnt + 1 / y_cnt))
+    z_score = (p_test - p_control) / se
+    p_value = 1 - norm.cdf(z_score)
+    return p_value < imp_crit
